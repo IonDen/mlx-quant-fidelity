b5fc09980e964fdd7623b3a57a5263d7d6545a1f
Denis Ineshin <denis.ineshin@gmail.com>
2026-06-15 18:31:38 +0200

    Export measure_weight_fidelity and WeightFidelityReport

--- src/mlx_quant_fidelity/__init__.py
+++ src/mlx_quant_fidelity/__init__.py
@@ -2,6 +2,13 @@
 
 from mlx_quant_fidelity._version import __version__
 from mlx_quant_fidelity.probes.kv import measure_kv_fidelity
-from mlx_quant_fidelity.report import FidelityReport
+from mlx_quant_fidelity.probes.weights import measure_weight_fidelity
+from mlx_quant_fidelity.report import FidelityReport, WeightFidelityReport
 
-__all__ = ["FidelityReport", "__version__", "measure_kv_fidelity"]
+__all__ = [
+    "FidelityReport",
+    "WeightFidelityReport",
+    "__version__",
+    "measure_kv_fidelity",
+    "measure_weight_fidelity",
+]
